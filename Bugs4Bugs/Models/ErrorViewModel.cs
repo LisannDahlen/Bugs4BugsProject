21f7177d381af19cdaec8f0def99dc52f24e7000
BergandDevOps <emiander92@gmail.com>
2023-03-24 09:29:48 +0100

    Add project files.

---
+++ Bugs4Bugs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bugs4Bugs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
