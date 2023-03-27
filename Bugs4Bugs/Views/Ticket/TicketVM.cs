0a0bcb135f9270a8abeefcee0d4ee1a6843d4a43
Zakk Elfving <Zakk.elfving@gmail.com>
2023-03-27 14:07:06 +0200

    Jobbat på ticketoverview

---
+++ Bugs4Bugs/Views/Ticket/TicketVM.cs
@@ -0,0 +1,17 @@
+﻿namespace Bugs4Bugs.Views.Ticket
+{
+    public class TicketOverviewVM
+    {
+        public string Title { get; set; }
+        public string Submiter { get; set; }
+        public string Status { get; set; }
+        public string BugType { get; set; }
+        public string Urgency { get; set; }
+        public string Submitted { get; set; }
+        public string LastUpdated { get; set; }
+        public string Product { get; set; }
+        public string Developer { get; set; }
+        public string Description { get; set; }
+
+    }
+}
