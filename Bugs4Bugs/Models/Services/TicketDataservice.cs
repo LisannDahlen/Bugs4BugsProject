b9eb624cad3467a52caa14729547856d636ca7d7
BergandDevOps <emiander92@gmail.com>
2023-03-28 08:55:31 +0200

    Ticket dataservice static list of tickets for testing

--- Bugs4Bugs/Models/Services/TicketDataservice.cs
+++ Bugs4Bugs/Models/Services/TicketDataservice.cs
@@ -1,4 +1,5 @@
 ﻿using Bugs4Bugs.Views.Ticket;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Bugs4Bugs.Models.Services
 {
@@ -33,30 +34,46 @@ namespace Bugs4Bugs.Models.Services
                             .ToArray();
         }
 
-        internal CreateTicketVM GetProductByName(string prodName)
+        internal CreateTicketVM? GetProductByName(string prodName)
         {
             return products.Where(p => p.Name == prodName)
-                .Select(p => new CreateTicketVM { ProductName = prodName , 
-                    BugTypes = p.GetBugtypesArray() ,
-                    UrgencyLevels = p.GetUrgencyArray()})
+                .Select(p => new CreateTicketVM
+                {
+                    ProductName = prodName,
+                    BugTypes = p.GetBugtypesArray(),
+                    UrgencyLevels = p.GetUrgencyArray()
+                })
                 .FirstOrDefault();
         }
-
-        List<Ticket> tickets = new List<Ticket>()
-        {
-            new Ticket()
+        public static List<Ticket> tickets = new List<Ticket> {
+            new Ticket
             {
                 Id = 1,
                 Title = "Mördarrobotar",
                 Description = "En smältande polis jagar mig och en Österrikisk bodybuilder säger att jag ska rädda framtiden",
                 SubmittedDate = DateTime.Now,
                 LastUpdated = DateTime.Now,
-                Submitter = new SiteUser() {FirstName="John", LastName="Connor", UserName = "JohnConnor" },
-                TicketProduct = products.FirstOrDefault(o => o.Name=="Skynet"),
-                TicketBugType = products.FirstOrDefault(o => o.Name=="Skynet").bugTypes[0],
-                TicketUrgency = products.FirstOrDefault(o => o.Name=="Skynet").urgencies[0],
-                TicketStatus =  products.FirstOrDefault(o => o.Name=="Skynet").statuses[0]
-            }
+                Submitter = new SiteUser{ FirstName = "John", LastName = "Connor", UserName = "JohnConnor" },
+                TicketProduct = products.FirstOrDefault(o => o.Name == "Skynet"),
+                TicketBugType = products.FirstOrDefault(o => o.Name == "Skynet").bugTypes[0],
+                TicketUrgency = products.FirstOrDefault(o => o.Name == "Skynet").urgencies[0],
+                TicketStatus = products.FirstOrDefault(o => o.Name == "Skynet").statuses[0]
+            },
+            new Ticket
+            {
+                Id = 1,
+                Title = "Programmet åt min läxa",
+                Description = "Jag gjorde min matteläxa när programmet plötsligt ",
+                SubmittedDate = DateTime.Now,
+                LastUpdated = DateTime.Now,
+                Submitter = new SiteUser{ FirstName = "Tommy", LastName = "Boy", UserName = "TommyBoy97" },
+                TicketProduct = products.FirstOrDefault(o => o.Name == "Firefox"),
+                TicketBugType = products.FirstOrDefault(o => o.Name == "Firefox").bugTypes[0],
+                TicketUrgency = products.FirstOrDefault(o => o.Name == "Firefox").urgencies[0],
+                TicketStatus = products.FirstOrDefault(o => o.Name == "Firefox").statuses[0]
+            },
         };
+
+
     }
 }
