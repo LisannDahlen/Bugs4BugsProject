6984b0906a23125a7872202b028674e15bcb4c7a
BergandDevOps <emiander92@gmail.com>
2023-03-28 15:33:14 +0200

    Urgency ctor created

--- Bugs4Bugs/Models/Urgency.cs
+++ Bugs4Bugs/Models/Urgency.cs
@@ -2,6 +2,15 @@
 {
     public class Urgency
     {
+        public Urgency()
+        {
+            
+        }
+        public Urgency(string selectedUrgencyLevel)
+        {
+            Level = selectedUrgencyLevel;
+        }
+
         public int Id { get; set; }
         public string Level { get; set; }
         public string? Description { get; set; }
