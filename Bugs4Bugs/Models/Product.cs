1d9e58b556dba868e2685fa11d9be9789c498a1d
BergandDevOps <emiander92@gmail.com>
2023-03-29 08:45:03 +0200

    New migration

--- Bugs4Bugs/Models/Product.cs
+++ Bugs4Bugs/Models/Product.cs
@@ -20,7 +20,7 @@
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public string PhotoURL { get; set; }    
         public List<SiteUserRole> ProductTeam { get; set; }
